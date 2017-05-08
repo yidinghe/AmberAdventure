ba5e764aff17112a4c650cc782724ca1ec038f89
yiding he <yidinghe2012@gmail.com>
2017-05-07 21:22:15 -0400

    Added SplashScreen and animation

---
+++ Assets/Scripts/Utility/GoToScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour {
+
+	public string sceneName = "";
+	public int sceneId = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GoTo (){
+		if (sceneName == "") {
+			SceneManager.LoadScene (sceneId);
+		} else {
+			SceneManager.LoadScene (sceneName);
+		}
+	}
+}

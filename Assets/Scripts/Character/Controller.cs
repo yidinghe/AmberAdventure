83a746e325fe5a97f91dea7616de49044f7f39e9
yiding he <yidinghe2012@gmail.com>
2017-05-09 22:01:19 -0400

    Added Assets.

---
+++ Assets/Scripts/Character/Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public float speed = 200f;
+	internal Rigidbody2D body;
+
+	void Awake(){
+		body = GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool upKey = Input.GetKeyUp (KeyCode.RightArrow) || Input.GetKeyUp (KeyCode.LeftArrow);
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			Move (1);
+			Direction(0);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			Move (-1);
+			Direction(1);
+		}
+
+		if (upKey) {
+			Move (0);
+		}
+
+	}
+
+	void Move(int i){
+		body.velocity = new Vector2(i*speed*Time.deltaTime, body.velocity.y) ;
+	}
+
+	void Direction(int i){
+		transform.eulerAngles = new Vector3 (0, 180f*i, 0);
+	}
+}

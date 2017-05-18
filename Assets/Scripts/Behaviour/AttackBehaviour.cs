1cddea228be852b71e6f5f2ed7d8f41f48878e4e
yiding he <yidinghe2012@gmail.com>
2017-05-17 22:00:37 -0400

    Added X button. TODO mobile support later.

--- Assets/Scripts/Behaviour/AttackBehaviour.cs
+++ Assets/Scripts/Behaviour/AttackBehaviour.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class AttackBehaviour : StateMachineBehaviour {
 
+	public float transhold = 1;
+	public int order = 0;
+
+	void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex){
+		if (animatorStateInfo.normalizedTime <= transhold) {
+			animator.SetInteger ("Attack", order);
+		}
+
+	}
+
 	void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex){
 		animator.SetInteger("Attack", 0) ;
 	}

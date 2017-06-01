a53e577224245ca53b0e1df0a8f40ddf5bc0cd7c
yiding he <yiding.he@samsungpay.com>
2017-06-01 16:16:28 -0400

    Update Mobile Control to support Large Screen Android Phones.

--- Assets/Scripts/Character/ControllerBattle.cs
+++ Assets/Scripts/Character/ControllerBattle.cs
@@ -30,33 +30,38 @@ public class ControllerBattle : Controller
 		}
 	}
 
-	public override void Move(int i){
+	public override void Move (int i)
+	{
 //		if(lockMove){
 //			body.velocity = new Vector2 (i * speed * Time.deltaTime, body.velocity.y);
 //		}	
 		body.velocity = new Vector2 (i * speed * Time.deltaTime, body.velocity.y);
 		anim.SetFloat ("Move", Mathf.Abs (i));
 	}
-		
-	public override void Direction(int i){
+
+	public override void Direction (int i)
+	{
 //		if (lockMove)
 //			return;
 		transform.eulerAngles = new Vector3 (0, 180f * i, 0);
 	}
 
-	public void OnKeyADown(){
+	public void OnKeyADown ()
+	{
 		Move (0);
 		lockMove = true;
 		anim.SetInteger ("Attack", anim.GetInteger ("Attack") + 1);
 	}
 
-	public void OnKeyBDown(){
+	public void OnKeyBDown ()
+	{
 		Move (0);
 		lockMove = true;
 		anim.SetTrigger ("Skill");
 	}
 
-	public void OnKeyXDown(){
+	public void OnKeyXDown ()
+	{
 		if (isShotStart) {
 			anim.SetBool ("Shot", false);	
 		} else {
